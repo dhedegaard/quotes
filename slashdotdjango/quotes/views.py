35ea480c06a131c3f1c65744eddb44bc25860de0
Dennis Hedegaard <neo2k@neo2k.dk>
2012-08-19 19:28:33 +0200

    initial search gui implementation.

--- slashdotdjango/quotes/views.py
+++ slashdotdjango/quotes/views.py
@@ -7,18 +7,23 @@ import math
 
 PAGE_SIZE = 20
 
-def _generate_pagelist(page, page_count, count=PAGE_SIZE):
+def _generate_pagelist(page, pagecount, count=PAGE_SIZE):
     '''
     Generates the numbers for the pages displayed in the gui.
     '''
-    if page < count / 2:
-        return range(1, count + 1)
-    elif page > page_count - (count / 2):
-        return range(page_count - count, page_count + 1)
+    if pagecount == 0:
+        return []
+    elif page < count / 2:
+        if pagecount > 10:
+            return range(1, 10 + 1)
+        else:
+            return range(1, pagecount)
+    elif page > pagecount - (count / 2):
+        return range(pagecount - count, pagecount + 1)
     else:
         return range(page - count / 2 + 1, page + count / 2 + 1)
 
-def search_quotes(search):
+def _search_quotes(search):
     '''
     Returns the quotes fitting the search-pattern based on searching
     according to a postgres searchvector (tsearch2).
@@ -38,26 +43,41 @@ def index(request, page=1):
     '''
     This method is used as a url handler for django.
     '''
+
+    # handle search parameter, if any
+    GET = request.GET
+    search = ''
+    if 'search' in GET:
+        search = GET['search'].strip()
+        if len(search) > 0:
+            # trim '+' in beginning and end, since that's just space escaped.
+            if search[0] == '+':
+                search = search[1:]
+            if search[-1] == '+':
+                search = search[:-1]
+    
     # convert page, if it's not an int (ie str)
     if not isinstance(page, int):
         page = int(page)
 
     # get data, setup content
     first_index = (page - 1) * PAGE_SIZE
-    quotes = Quote.objects.all().order_by('-created')[first_index:first_index + PAGE_SIZE]
-    pagecount = int(math.ceil(len(Quote.objects.all()) / float(PAGE_SIZE)))
+    if search:
+        quotes = _search_quotes(search)
+    else:
+        quotes = Quote.objects.all().order_by('-created')
+
+    pagecount = int(math.ceil(quotes.count() / float(PAGE_SIZE))) + 1
+    quotes = quotes[first_index:first_index + PAGE_SIZE]
 
     # if page above pagecount, then 404.
-    if page > pagecount:
+    if page > pagecount and pagecount > 0:
         raise Http404
 
+    # setup the rest of the variables
+    pageprev = page - 1
+    pagenext = page + 1
+    pagelist = _generate_pagelist(page, pagecount)
+
     # return render from template
-    return render_to_response(
-        'quotes/index.html',
-        {'quotes': quotes,
-         'page': page,
-         'pageprev': page - 1,
-         'pagenext': page + 1,
-         'pagelist': _generate_pagelist(page, pagecount),
-         'pagecount': pagecount,
-         })
+    return render_to_response('quotes/index.html', locals())
