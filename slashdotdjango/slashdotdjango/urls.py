fe372f0f3fd03912c7a99dc25279441a020a80d4
Dennis Hedegaard <neo2k@neo2k.dk>
2012-09-01 16:41:15 +0200

    bootstrap ui

--- slashdotdjango/slashdotdjango/urls.py
+++ slashdotdjango/slashdotdjango/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^random', 'quotes.views.random'),
     url(r'^page/([1-9]\d*)/', 'quotes.views.index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', 'quotes.views.index'),
