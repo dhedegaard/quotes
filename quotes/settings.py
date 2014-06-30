079a593fdb5cdd58ed3c505ea2790b81600b80e6
Dennis Hedegaard <dennis@dhedegaard.dk>
2014-06-30 22:36:10 +0200

    set the explicit testrunner to use.

--- quotes/settings.py
+++ quotes/settings.py
@@ -146,6 +146,8 @@ if DEBUG:
     l.setLevel(logging.DEBUG)
     l.addHandler(logging.StreamHandler())
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
