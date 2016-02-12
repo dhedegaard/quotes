f76242f775103c90adebd55ffd2ecc5313a601ce
Dennis Hedegaard <dennis@dhedegaard.dk>
2016-02-13 00:07:33 +0100

    fixed url patterns to new Django 1.8 style.

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from django.contrib import admin
 admin.autodiscover()
 
 from . import views, rest
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^rest/random/$', rest.rest_random, name='rest_random'),
     url(r'^rest/latest/$', rest.rest_latest, name='rest_latest'),
     url(r'^random/$', views.random, name='random'),
     url(r'^$', views.index, name='index'),
-)
+]
